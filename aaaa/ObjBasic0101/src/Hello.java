3db924f501702297c359d9790b59d2e20897c136
github shumpei-kinoshita <kinoshita.shumpei@dc.kyocera.com>
2025-06-05 11:23:07 +0900

    二回目コミット

---
+++ aaaa/ObjBasic0101/src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * eclipseサンプル
+ * @author M.Takahashi
+ */
+public class Hello {
+	/**
+	 * 表示のサンプル
+	 * @param args 未使用
+	 */
+	public static void main(String[] args) {
+		System.out.println("eclipseの世界へ、ようこそ。");
+	}
+}
